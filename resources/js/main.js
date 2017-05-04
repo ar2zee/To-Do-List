11844a1273bdd4b8bbbf71b70f6240a574defa93
Artur Oganesyan <artur.og@yahoo.com>
2017-05-04 00:19:19 -0500

    added buttons to our list and created JS file

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+
+function buttonClick() {
+  var value = document.getElementById('item').value;
+
+  if (value) {
+    
+  }
+
+}
+
+
+document.getElementById('add').addEventListener('click', buttonClick);
